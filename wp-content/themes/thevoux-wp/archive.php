68502824772c764067f55f8303abd1cb92e7f3d0
The-Comet <YKFDDZmPjdyTBfSkpByE>
2019-09-20 00:23:20 +1000

    Update project 09.20 12.23

--- wp-content/themes/thevoux-wp/archive.php
+++ wp-content/themes/thevoux-wp/archive.php
@@ -2,5 +2,4 @@
 <?php get_template_part( 'inc/templates/header/archive-title' ); ?>
 <?php $archive_layout = ot_get_option( 'archive_layout', 'style1' ); ?>
 <?php get_template_part( 'inc/templates/archive/' . $archive_layout ); ?>
-<?php
-get_footer();
+<?php get_footer();
